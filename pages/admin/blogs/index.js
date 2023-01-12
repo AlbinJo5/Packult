4d9d0a072690b901c4c04f80068d8a454ba2c3fb
AlbinJo5 <albinjo12345alex@gmail.com>
2023-01-12 09:52:00 +0530

    all blogs

--- pages/admin/blogs/index.js
+++ pages/admin/blogs/index.js
@@ -7,7 +7,12 @@ function index() {
         <AdminLayout>
             <div className={styles.blogs}>
                 <h2>Blogs</h2>
+                <hr/>
                 <div className={styles.all_blogs}>
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
+                    <BlogCard />
                     <BlogCard />
                 </div>
 
