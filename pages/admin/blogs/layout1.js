2c967a8f080cd3f381bc8e8ac256a7027c2d3592
AlbinJo5 <albinjo12345alex@gmail.com>
2023-01-13 08:42:42 +0530

    layout 1 add

--- pages/admin/blogs/layout1.js
+++ pages/admin/blogs/layout1.js
@@ -2,13 +2,20 @@ import Image from "next/image"
 import AdminLayout from "../../../components/admin/adminLayout"
 import styles from "../../../styles/admin/components/layouts.module.scss"
 import { useState } from "react"
+import { db } from "../../../utils/firebase.js"
+import { collection } from "firebase/firestore"
 
 function Index() {
 
-    const [image1, setImage1] = useState("/assets/admin/dummyImage.jpg")
-    const [image2, setImage2] = useState("/assets/admin/dummyImage.jpg")
-    const [image3, setImage3] = useState("/assets/admin/dummyImage.jpg")
-    const [image4, setImage4] = useState("/assets/admin/dummyImage.jpg")
+    const [image1Url, setImage1Url] = useState("/assets/admin/dummyImage.jpg")
+    const [image2Url, setImage2Url] = useState("/assets/admin/dummyImage.jpg")
+    const [image3Url, setImage3Url] = useState("/assets/admin/dummyImage.jpg")
+    const [image4Url, setImage4Url] = useState("/assets/admin/dummyImage.jpg")
+
+    const [image1, setImage1] = useState("")
+    const [image2, setImage2] = useState("")
+    const [image3, setImage3] = useState("")
+    const [image4, setImage4] = useState("")
 
     const [title, setTitle] = useState("")
     const [date, setDate] = useState("")
@@ -24,68 +31,114 @@ function Index() {
     const [para9, setPara9] = useState("")
     const [para10, setPara10] = useState("")
 
-    const handleImage1 = (e) => {
+    const handleImage1 = (e)  =>  {
         if (e.target.files[0]) {
-            setImage1(URL.createObjectURL(e.target.files[0]))
+            setImage1(e.target.files[0])
+            setImage1Url(URL.createObjectURL(e.target.files[0]))
         }
     }
 
     const handleImage2 = (e) => {
         if (e.target.files[0]) {
-            setImage2(URL.createObjectURL(e.target.files[0]))
+            setImage2(e.target.files[0])
+            setImage2Url(URL.createObjectURL(e.target.files[0]))
         }
     }
 
     const handleImage3 = (e) => {
         if (e.target.files[0]) {
-            setImage3(URL.createObjectURL(e.target.files[0]))
+            setImage3(e.target.files[0])
+            setImage3Url(URL.createObjectURL(e.target.files[0]))
         }
     }
 
     const handleImage4 = (e) => {
         if (e.target.files[0]) {
-            setImage4(URL.createObjectURL(e.target.files[0]))
+            setImage4(e.target.files[0])
+            setImage4Url(URL.createObjectURL(e.target.files[0]))
         }
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
         // check for empty fields with images
-        if (title === "" || date === "" || para1 === "" || para2 === "" || para3 === "" || para4 === "" || para5 === "" || para6 === "" || para7 === "" || para8 === "" || para9 === "" || para10 === "" || image1 === "/assets/admin/dummyImage.jpg" || image2 === "/assets/admin/dummyImage.jpg" || image3 === "/assets/admin/dummyImage.jpg" || image4 === "/assets/admin/dummyImage.jpg") {
+        if (image1 === "" || image2 === "" || image3 === "" || image4 === "" || title === "" || date === "" || para1 === "" || para2 === "" || para3 === "" || para4 === "" || para5 === "" || para6 === "" || para7 === "" || para8 === "" || para9 === "" || para10 === "") {
             alert("Please fill all the fields")
-        } else {
-            // upload the data to the database
-            const data = {
-                title,
-                date,
-                para1,
-                para2,
-                para3,
-                para4,
-                para5,
-                para6,
-                para7,
-                para8,
-                para9,
-                para10,
-                image1,
-                image2,
-                image3,
-                image4,
-                layout: "layout1"
-            }
-            console.log(data)
+            return
         }
+
+        // create a ref to firestore
+        const blogRef = doc(collection(db, "blogs"))
+
+        // upload images to storage using the ref
+        const uploadTask1 = ref(storage, `blogs/${blogRef.id}/image1`).put(image1)
+        const uploadTask2 = ref(storage, `blogs/${blogRef.id}/image2`).put(image2)
+        const uploadTask3 = ref(storage, `blogs/${blogRef.id}/image3`).put(image3)
+        const uploadTask4 = ref(storage, `blogs/${blogRef.id}/image4`).put(image4)
+
+        // get the download url of the images
+        const downloadUrl1 = getDownloadURL(uploadTask1.snapshot.ref)
+        const downloadUrl2 = getDownloadURL(uploadTask2.snapshot.ref)
+        const downloadUrl3 = getDownloadURL(uploadTask3.snapshot.ref)
+        const downloadUrl4 = getDownloadURL(uploadTask4.snapshot.ref)
+
+        // create a blog object
+        const blogDetails = {
+            title: title,
+            date: date,
+            image1: downloadUrl1,
+            image2: downloadUrl2,
+            image3: downloadUrl3,
+            image4: downloadUrl4,
+            para1: para1,
+            para2: para2,
+            para3: para3,
+            para4: para4,
+            para5: para5,
+            para6: para6,
+            para7: para7,
+            para8: para8,
+            para9: para9,
+            para10: para10,
+        }
+        // split the words by 30
+         const para1Words = para1.split(" ")
+         const para1Words30 = para1Words.slice(0, 30)
+         const para1Words30String = para1Words30.join(" ")
+         const para1Words30StringWithDots = para1Words30String + "..."
+
+        // create a blog with only title, date, mainImage, desc  and layout, and rest should be inside a details collection
+        const blog = {
+            title: title,
+            date: date,
+            mainImage: downloadUrl1,
+            description: para1Words30StringWithDots,
+            layout: "layout1"
+        }
+        
+        // add the blog to firestore
+        setDoc(blogRef, blog)
+            .then(() => {
+                // add the blog details to the details collection
+                const blogDetailsRef = doc(collection(db, "blogs", blogRef.id, "details"))
+                setDoc(blogDetailsRef, blogDetails)
+                    .then(() => {
+                        alert("Blog added successfully")
+                    })
+                    .catch((error) => {
+                        console.log(error)
+                    })
+            })
+
+
     }
-
-
 
     return (
         <AdminLayout>
             <div className={styles.layout1} >
                 <form onSubmit={handleSubmit}>
                     <label htmlFor="image1">
-                        <Image src={image1} id="layout1_image1" name="image1" height={1000} width={1000} alt="dummyImage" />
+                        <Image src={image1Url} id="layout1_image1" name="image1" height={1000} width={1000} alt="dummyImage" />
                     </label>
                     <input type="file" id="image1" onChange={handleImage1} />
                     <div className={styles.head}>
@@ -100,10 +153,10 @@ function Index() {
 
 
                             <label htmlFor="image2">
-                                <Image src={image2} name="image2" height={1000} width={1000} alt="dummyImage" />
+                                <Image src={image2Url} name="image2" height={1000} width={1000} alt="dummyImage" />
                             </label>
                             <label htmlFor="image3">
-                                <Image src={image3} name="image3" height={1000} width={1000} alt="dummyImage" />
+                                <Image src={image3Url} name="image3" height={1000} width={1000} alt="dummyImage" />
                             </label>
                             <input type="file" id="image2" onChange={handleImage2} />
                             <input type="file" id="image3" onChange={handleImage3} />
@@ -115,7 +168,7 @@ function Index() {
                         <textarea onChange={(e) => setPara6(e.target.value)} placeholder="There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc." name="para6" id="para6" cols="30" rows="10"></textarea>
 
                         <label htmlFor="image4">
-                            <Image src={image4} name="image4" height={1000} width={1000} alt="dummyImage" />
+                            <Image src={image4Url} name="image4" height={1000} width={1000} alt="dummyImage" />
                         </label>
                         <input type="file" id="image4" onChange={handleImage4} />
 
