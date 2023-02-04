d7c767a160d2406e10ee0ca88429205d95514ad9
AlbinJo5 <albinjo12345alex@gmail.com>
2023-02-04 23:26:58 +0530

    cursor

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,26 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from 'react';
 
-function MyApp({ Component, pageProps }) {
 
+function MyApp({ Component, pageProps }) {
+  const cursor = document.querySelector("#cursor");
+  window.addEventListener("mousemove", (e) => {
+    console.log(e);
+    cursor.style.left = e.pageX + "px";
+    cursor.style.top = e.pageY + "px";
+  });
   useEffect(() => {
     AOS.init();
   }, []);
 
-  return <Component {...pageProps} />
+  return (
+    <>
+      <div id="cursor" >
+        <span id="cursorSpan"></span>
+      </div>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
