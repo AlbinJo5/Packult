501ad7e5add09cd9f9554685b0b98f0a4a9c3859
AlbinJo5 <albinjo12345alex@gmail.com>
2023-02-21 16:49:21 +0530

    Change 4

--- pages/api/our-work/get.js
+++ pages/api/our-work/get.js
@@ -14,6 +14,7 @@ export default async function handler(req, res) {
         });
         res.status(200).json(posts);
     } catch (error) {
+        console.log(error);
         res.status(500).json({ error: error });
     }
 }
