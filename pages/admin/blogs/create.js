b1392d251ddd3303b9689967766cbd501bdeca52
Albin Jo Alex <75153651+AlbinJo5@users.noreply.github.com>
2023-02-22 18:31:53 +0530

    Blog Spelling error

--- pages/admin/blogs/create.js
+++ pages/admin/blogs/create.js
@@ -10,7 +10,7 @@ function Create() {
             <div className={styles.create} >
                 <h2>Choose layout</h2>
                 <div className={styles.layouts} >
-                    <Image onClick={() => { router.push(ADMIN_ROUTES.BLOGS_LAYOUT_1) }} src="/assets/admin/BLog1.png" height={1000} width={1000} alt="layout1" />
+                    <Image onClick={() => { router.push(ADMIN_ROUTES.BLOGS_LAYOUT_1) }} src="/assets/admin/Blog1.png" height={1000} width={1000} alt="layout1" />
                     <Image onClick={() => { router.push(ADMIN_ROUTES.BLOGS_LAYOUT_2) }} src="/assets/admin/Blog2.png" height={1000} width={1000} alt="layout2" />
                     <Image onClick={() => { router.push(ADMIN_ROUTES.BLOGS_LAYOUT_3) }} src="/assets/admin/Blog3.png" height={1000} width={1000} alt="layout3" />
                 </div>
